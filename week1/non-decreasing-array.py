67543129984a4aa836dba669be60100ada886610
selome <106982265+Selomegech@users.noreply.github.com>
2023-12-11 05:40:01 +0300

    Added solution for Non-decreasing Array

---
+++ week1/non-decreasing-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        n = len(nums)
+        modified = False
+        
+        for i in range(n - 1):
+            if nums[i] > nums[i+1]:
+                if modified:
+                    return False
+                
+                if i > 0 and nums[i-1] > nums[i+1]:
+                    nums[i+1] = nums[i]
+                else:
+                    nums[i] = nums[i+1]
+                
+                modified = True
+        
+        return True
