8648e455a3f0f28c43006ac5f9c56bd21ba5d182
selome <106982265+Selomegech@users.noreply.github.com>
2024-02-26 08:55:42 +0300

    Add solution for Partition Labels

--- a2sv/leetcode/partition-labels.py
+++ a2sv/leetcode/partition-labels.py
@@ -1,18 +1,18 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last_occurrence = {}
-        for i, char in enumerate(s):
-            last_occurrence[char] = i
+        last_occurance = {}
 
-        result = []
-        start = 0
+        for i,char in enumerate(s):
+            last_occurance[char] = i
+        start =0
         end = 0
+        result = []
 
-        for i, char in enumerate(s):
-            end = max(end, last_occurrence[char])
+        for i,char in enumerate(s):
+            end = max(end , last_occurance[char])
 
-            if i == end:
+            if i == end :
                 result.append(end - start + 1)
                 start = i + 1
-
-        return result
+        return result 
+        
